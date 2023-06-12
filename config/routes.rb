47ffd364a4cf79d021ae9e93c50e00bc49d54a9f
JasonStaubach <jbstaubach@gmail.com>
2023-06-12 16:02:45 -0400

    session fixes

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
   end
 
   resources :users, only: [:new, :create]
-  resource :sessions, only: [:new,:create,:destroy]
+  resource :session, only: [:new,:create,:destroy]
 
 
 end
